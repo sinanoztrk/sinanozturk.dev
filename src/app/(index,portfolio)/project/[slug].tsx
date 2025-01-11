119be26198b7fb977c72f376bfc84bd2331b438d
Sinan Öztürk <sinanozturkk@icloud.com>
2025-01-11 20:47:33 +0300

    Enhance portfolio and project pages with SEO improvements and layout updates
    - Added <Head> components to index, portfolio, and project pages for better SEO, including titles and meta descriptions.
    - Refactored layout structure using <Fragment> for improved readability and organization.
    - Updated personal information and project details for clarity and relevance.
    - Removed unused favicon image to streamline asset management.
    
    This commit improves the overall user experience and search engine visibility of the portfolio website.

--- src/app/(index,portfolio)/project/[slug].tsx
+++ src/app/(index,portfolio)/project/[slug].tsx
@@ -5,6 +5,18 @@ import * as AC from "@bacons/apple-colors";
 import { PortfolioItems } from "@/constants/Portfolio";
 import { useGlobalSearchParams } from "expo-router";
 import { PortfolioList } from "@/components/PortfolioList";
+import { Fragment } from "react";
+import Head from "expo-router/head";
+
+export async function generateStaticParams(): Promise<
+  Record<string, string>[]
+> {
+  // Return an array of params to generate static HTML files for.
+  // Each entry in the array will be a new page.
+  return PortfolioItems.map((item) => ({
+    slug: (item.href as any)?.params?.slug,
+  }));
+}
 
 export default function PortfolioItem() {
   const { slug } = useGlobalSearchParams<{ slug: string }>();
@@ -13,83 +25,91 @@ export default function PortfolioItem() {
   );
 
   return (
-    <View
-      style={{
-        flex: 1,
-        width: "100%",
-        maxWidth: 650,
-        marginHorizontal: "auto",
-      }}
-    >
-      <Form.List navigationTitle={project?.title}>
-        <Form.Section>
-          <View style={{ alignItems: "center", gap: 8, padding: 16, flex: 1 }}>
-            <Image
-              source={project?.image}
-              style={{
-                aspectRatio: 1,
-                width: "100%",
-                borderRadius: 8,
-                marginBottom: 10,
-              }}
-            />
-            <Form.Text
-              style={{
-                fontSize: 20,
-                fontWeight: "600",
-              }}
+    <Fragment>
+      <Head>
+        <title>{project?.title} - Sinan Öztürk</title>
+        <meta name="description" content={project?.summary} />
+      </Head>
+      <View
+        style={{
+          flex: 1,
+          width: "100%",
+          maxWidth: 650,
+          marginHorizontal: "auto",
+        }}
+      >
+        <Form.List navigationTitle={project?.title}>
+          <Form.Section>
+            <View
+              style={{ alignItems: "center", gap: 8, padding: 16, flex: 1 }}
             >
-              {project?.title}
-            </Form.Text>
-            <Form.Text
-              style={{
-                textAlign: "center",
-                fontSize: 14,
-                color: AC.secondaryLabel,
-              }}
-            >
-              {project?.summary}
-            </Form.Text>
-          </View>
-        </Form.Section>
-
-        {project?.links && project?.links?.length > 0 && (
-          <Form.Section title="Bağlantılar">
-            {project?.links?.map((link, index) => (
-              <Form.Link key={index} href={link.href} target="_blank">
-                {link.title}
-              </Form.Link>
-            ))}
-          </Form.Section>
-        )}
-
-        <Form.Section title="Özellikler">
-          {project?.features?.map((feature, index) => (
-            <View key={index}>
-              <Form.Text>{feature}</Form.Text>
-            </View>
-          ))}
-        </Form.Section>
-
-        {project?.images && project?.images?.length > 0 && (
-          <Form.Section title="Resimler">
-            {project?.images?.map((image, index) => (
               <Image
-                key={index}
-                source={image}
-                style={{ aspectRatio: 1, width: "100%" }}
+                source={project?.image}
+                style={{
+                  aspectRatio: 1,
+                  width: "100%",
+                  borderRadius: 8,
+                  marginBottom: 10,
+                }}
               />
+              <Form.Text
+                style={{
+                  fontSize: 20,
+                  fontWeight: "600",
+                }}
+              >
+                {project?.title}
+              </Form.Text>
+              <Form.Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 14,
+                  color: AC.secondaryLabel,
+                }}
+              >
+                {project?.summary}
+              </Form.Text>
+            </View>
+          </Form.Section>
+
+          {project?.links && project?.links?.length > 0 && (
+            <Form.Section title="Bağlantılar">
+              {project?.links?.map((link, index) => (
+                <Form.Link key={index} href={link.href} target="_blank">
+                  {link.title}
+                </Form.Link>
+              ))}
+            </Form.Section>
+          )}
+
+          <Form.Section title="Özellikler">
+            {project?.features?.map((feature, index) => (
+              <View key={index}>
+                <Form.Text>{feature}</Form.Text>
+              </View>
             ))}
           </Form.Section>
-        )}
 
-        <PortfolioList
-          title="Diğer Projeler"
-          data={PortfolioItems?.filter(
-            (item) => (item?.href as any)?.params?.slug !== slug
+          {project?.images && project?.images?.length > 0 && (
+            <Form.Section title="Resimler">
+              {project?.images?.map((image, index) => (
+                <Image
+                  key={index}
+                  source={image}
+                  style={{ aspectRatio: 1, width: "100%" }}
+                />
+              ))}
+            </Form.Section>
           )}
-        />
-      </Form.List>
-    </View>
+
+          <PortfolioList
+            title="Diğer Projeler"
+            data={PortfolioItems?.filter(
+              (item) => (item?.href as any)?.params?.slug !== slug
+            )}
+          />
+        </Form.List>
+      </View>
+    </Fragment>
   );
 }
