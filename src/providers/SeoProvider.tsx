ae8b3479cab351836ab747583dd407a924435add
Sinan Öztürk <sinanozturkk@icloud.com>
2025-01-12 16:23:04 +0300

    Update cover images and enhance SEO metadata generation in SeoProvider
    - Replaced cover images for multiple projects to ensure up-to-date visuals.
    - Refactored SeoProvider to dynamically generate meta tags for improved SEO across portfolio and project pages.
    - Introduced a new function for generating meta tags, enhancing clarity and consistency in SEO handling.
    - Streamlined metadata organization to better reflect project-specific information and improve social media sharing capabilities.

--- src/providers/SeoProvider.tsx
+++ src/providers/SeoProvider.tsx
@@ -3,45 +3,66 @@ import Head from "expo-router/head";
 import { usePathname } from "expo-router";
 import { Projects } from "@/constants/Projects";
 
+interface MetaTagsProps {
+  title: string;
+  description: string;
+  image: string;
+  url: string;
+  type?: "website" | "article";
+}
+
 export default function SeoProvider(props: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const seoMetadata = {
-    "/": {
-      title: "Sinan Öztürk - Software Engineer",
-      description:
-        "İstanbul'da yaşayan 25 yaşında bir yazılım mühendisiyim. Mobil ve web frontend geliştirme konusunda uzmanım. Boş zamanlarımda backend ve frontend projelerle ilgileniyorum. Kullanıcı odaklı ve işlevsel uygulamalar geliştirerek şirketlerle çalışıyorum. Kendimi sürekli geliştirmeye ve yeni teknolojilere açık olmaya özen gösteriyorum.",
-      image: "https://sinanozturk.dev/images/avatar.png",
-    },
-    "/portfolio": {
-      title: "Sinan Öztürk - Portfolyo",
-      description:
-        "İstanbul'da yaşayan 25 yaşında bir yazılım mühendisiyim. Mobil ve web frontend geliştirme konusunda uzmanım. Boş zamanlarımda backend ve frontend projelerle ilgileniyorum. Kullanıcı odaklı ve işlevsel uygulamalar geliştirerek şirketlerle çalışıyorum. Kendimi sürekli geliştirmeye ve yeni teknolojilere açık olmaya özen gösteriyorum.",
-      image: "https://sinanozturk.dev/images/avatar.png",
-    },
-    "/project/[slug]": {
-      title: "Sinan Öztürk - Portfolyo",
-      description:
-        "İstanbul'da yaşayan 25 yaşında bir yazılım mühendisiyim. Mobil ve web frontend geliştirme konusunda uzmanım. Boş zamanlarımda backend ve frontend projelerle ilgileniyorum. Kullanıcı odaklı ve işlevsel uygulamalar geliştirerek şirketlerle çalışıyorum. Kendimi sürekli geliştirmeye ve yeni teknolojilere açık olmaya özen gösteriyorum.",
-      image: "https://sinanozturk.dev/images/avatar.png",
-    },
+  const baseUrl = "https://sinanozturk.dev";
+
+  const generateMetaTags = ({
+    title,
+    description,
+    image,
+    url,
+    type = "website",
+  }: MetaTagsProps) => {
+    return (
+      <>
+        {/* HTML Meta Tags */}
+        <title>{title}</title>
+        <meta name="description" content={description} />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content={type} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="sinanozturk.dev" />
+        <meta property="twitter:url" content={url} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
+      </>
+    );
   };
 
   const renderSEOMetadata = () => {
     if (pathname == "/portfolio") {
+      const description =
+        "İstanbul'da yaşayan 25 yaşında bir yazılım mühendisiyim. Mobil ve web frontend geliştirme konusunda uzmanım. Boş zamanlarımda backend ve frontend projelerle ilgileniyorum. Kullanıcı odaklı ve işlevsel uygulamalar geliştirerek şirketlerle çalışıyorum. Kendimi sürekli geliştirmeye ve yeni teknolojilere açık olmaya özen gösteriyorum.";
+      const title = "Sinan Öztürk - Portfolyo";
+      const image = `${baseUrl}/images/avatar.png`;
+
       return (
         <Head>
-          <title>Sinan Öztürk - Portfolyo</title>
-          <meta
-            name="description"
-            property="og:description"
-            content={
-              "İstanbul'da yaşayan 25 yaşında bir yazılım mühendisiyim. Mobil ve web frontend geliştirme konusunda uzmanım. Boş zamanlarımda backend ve frontend projelerle ilgileniyorum. Kullanıcı odaklı ve işlevsel uygulamalar geliştirerek şirketlerle çalışıyorum. Kendimi sürekli geliştirmeye ve yeni teknolojilere açık olmaya özen gösteriyorum."
-            }
-          />
-          <meta
-            property="og:image"
-            content="https://sinanozturk.dev/images/avatar.png"
-          />
+          {generateMetaTags({
+            title,
+            description,
+            image,
+            url: `${baseUrl}/portfolio`,
+          })}
         </Head>
       );
     }
@@ -51,37 +72,36 @@ export default function SeoProvider(props: { children: React.ReactNode }) {
       const project = Projects.find(
         (item) => (item.href as any)?.params?.slug === slug
       );
+      const title = `${project?.title} - Sinan Öztürk`;
+      const description = project?.summary?.tr!;
+      const image = `${baseUrl}/images/${slug}/cover.png`;
 
       return (
         <Head>
-          <title>{project?.title} - Sinan Öztürk</title>
-          <meta
-            name="description"
-            property="og:description"
-            content={project?.summary?.tr}
-          />
-          <meta
-            property="og:image"
-            content={`https://sinanozturk.dev/images/${slug}/cover.png`}
-          />
+          {generateMetaTags({
+            title,
+            description,
+            image,
+            url: `${baseUrl}/project/${slug}`,
+            type: "article",
+          })}
         </Head>
       );
     }
 
+    const description =
+      "İstanbul'da yaşayan 25 yaşında bir yazılım mühendisiyim. Mobil ve web frontend geliştirme konusunda uzmanım. Boş zamanlarımda backend ve frontend projelerle ilgileniyorum. Kullanıcı odaklı ve işlevsel uygulamalar geliştirerek şirketlerle çalışıyorum. Kendimi sürekli geliştirmeye ve yeni teknolojilere açık olmaya özen gösteriyorum.";
+    const title = "Sinan Öztürk - Software Engineer";
+    const image = `${baseUrl}/images/avatar.png`;
+
     return (
       <Head>
-        <title>Sinan Öztürk - Software Engineer</title>
-        <meta
-          name="description"
-          property="og:description"
-          content={
-            "İstanbul'da yaşayan 25 yaşında bir yazılım mühendisiyim. Mobil ve web frontend geliştirme konusunda uzmanım. Boş zamanlarımda backend ve frontend projelerle ilgileniyorum. Kullanıcı odaklı ve işlevsel uygulamalar geliştirerek şirketlerle çalışıyorum. Kendimi sürekli geliştirmeye ve yeni teknolojilere açık olmaya özen gösteriyorum."
-          }
-        />
-        <meta
-          property="og:image"
-          content="https://sinanozturk.dev/images/avatar.png"
-        />
+        {generateMetaTags({
+          title,
+          description,
+          image,
+          url: baseUrl,
+        })}
       </Head>
     );
   };
