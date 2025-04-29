e2027d9f5a12782436b522199e69031486b76b94
Sinan Öztürk <sinanozturkk@icloud.com>
2025-04-29 10:39:43 +0300

    Enhance layout with dynamic header background based on color scheme
    - Integrated BlurView and useColorScheme for adaptive UI.
    - Updated Stack.Screen options to include a dynamic header background that adjusts to the system's grouped background color.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,32 @@
 import ThemeProvider from "@/components/ui/ThemeProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Stack from "@/components/ui/Stack";
+import { BlurView } from "expo-blur";
+import { useColorScheme, View } from "react-native";
+import * as AC from "@bacons/apple-colors";
 
 const queryClient = new QueryClient();
 
 export default function Layout() {
+  const colorScheme = useColorScheme();
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <Stack>
-          <Stack.Screen name="index" options={{ title: "" }} />
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "",
+              headerBackground() {
+                return (
+                  <View
+                    style={{ flex: 1, backgroundColor: AC.systemGroupedBackground }}
+                  />
+                );
+              },
+            }}
+          />
           <Stack.Screen
             name="project/[slug]"
             options={{ title: "Project" }}
